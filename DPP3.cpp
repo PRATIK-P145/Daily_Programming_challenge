6da8307c3b620d497654a9d107f8aa2a36e8bfbc
Pratik <pratikpatil0307@gmail.com>
2024-09-11 23:47:15 +0530

    Update DPP3.cpp
    DAY 3 Challenge Completed

--- DPP3.cpp
+++ DPP3.cpp
@@ -5,30 +5,25 @@ int findDuplicate(int arr[]) {
     
     int slow = arr[0];
     int fast = arr[0];
-
     
     do {
         slow = arr[slow];      
         fast = arr[arr[fast]];        
     } while (slow != fast);         
 
- 
     slow = arr[0];
 
-    
     while (slow != fast) {
         slow = arr[slow];     
         fast = arr[fast];             
     }
 
-   
     return fast;
 }
 
 int main() {
 
     int arr[] = {4,3,5,2,1,6,2};
-
 
     int duplicate = findDuplicate(arr);
     cout << "Duplicate number: " << duplicate << endl;
